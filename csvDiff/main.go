99b42ec51adb887498748d4f7b0b269f6a1c39bc
SOGAKEN <sogaken0919@gmail.com>
2023-11-30 16:05:57 +0900

    unique

--- csvDiff/main.go
+++ csvDiff/main.go
@@ -7,76 +7,73 @@ import (
 )
 
 func main() {
-	// 各CSVファイルからinum値を読み込む
-	aInums, err := readCsv("a.csv")
+	// 各CSVファイルからデータを読み込む
+	aData, header, err := readCsv("a.csv")
 	if err != nil {
 		panic(err)
 	}
-	bInums, err := readCsv("b.csv")
+	bData, _, err := readCsv("b.csv")
 	if err != nil {
 		panic(err)
 	}
 
 	// リストを比較して結果を得る
-	aUnique, bUnique, common := compareLists(aInums, bInums)
+	aUnique, bUnique, common := compareLists(aData, bData)
 
 	// 結果をCSVファイルに出力する
-	writeCsv("a_unique.csv", aUnique)
-	writeCsv("b_unique.csv", bUnique)
-	writeCsv("common.csv", common)
+	writeCsv("a_unique.csv", aUnique, header)
+	writeCsv("b_unique.csv", bUnique, header)
+	writeCsv("common.csv", common, header)
 
 	fmt.Println("CSV files have been written successfully.")
 }
 
-// readCsv はCSVファイルを読み込み、inum値のスライスを返す
-func readCsv(filename string) ([]string, error) {
+// readCsv はCSVファイルを読み込み、データのスライスとヘッダーを返す
+func readCsv(filename string) ([][]string, []string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer file.Close()
 
 	reader := csv.NewReader(file)
 	records, err := reader.ReadAll()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	var inums []string
-	for _, record := range records {
-		inums = append(inums, record[0]) // 'inum'が最初の列であると仮定
-	}
+	header := records[0] // ヘッダーを取得
+	data := records[1:]  // ヘッダーを除いたデータ
 
-	return inums, nil
+	return data, header, nil
 }
 
-// compareLists は2つのスライスを比較し、ユニークな要素と共通要素を返す
-func compareLists(a, b []string) (aUnique, bUnique, common []string) {
+// compareLists は2つのデータセットを比較し、ユニークな行と共通行を返す
+func compareLists(a, b [][]string) (aUnique, bUnique, common [][]string) {
 	mA := make(map[string]bool)
-	mB := make(map[string]bool)
+	mB := make(map[string][]string)
 
-	for _, item := range a {
-		mA[item] = true
-	}
 	for _, item := range b {
-		mB[item] = true
-		if _, found := mA[item]; found {
-			common = append(common, item)
+		mB[item[0]] = item // 'inum'が最初の列であると仮定
+	}
+	for _, item := range a {
+		if bItem, found := mB[item[0]]; found {
+			common = append(common, bItem)
 		} else {
-			bUnique = append(bUnique, item)
+			aUnique = append(aUnique, item)
 		}
 	}
-	for _, item := range a {
-		if _, found := mB[item]; !found {
-			aUnique = append(aUnique, item)
+	for _, item := range b {
+		if _, found := mA[item[0]]; !found {
+			bUnique = append(bUnique, item)
 		}
 	}
 
 	return
 }
 
-// writeCsv は指定されたファイル名でスライスをCSVファイルに書き込む
-func writeCsv(filename string, data []string) {
+// writeCsv は指定されたファイル名でデータセットをヘッダー付きでCSVファイルに書き込む
+func writeCsv(filename string, data [][]string, header []string) {
 	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
@@ -86,8 +83,13 @@ func writeCsv(filename string, data []string) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	// ヘッダーを書き込む
+	if err := writer.Write(header); err != nil {
+		panic("Error writing header to csv:" + err.Error())
+	}
+
 	for _, value := range data {
-		if err := writer.Write([]string{value}); err != nil {
+		if err := writer.Write(value); err != nil {
 			panic("Error writing record to csv:" + err.Error())
 		}
 	}
