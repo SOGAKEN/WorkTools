e187ad6778230cbff3847035750c6c0555eb7382
SOGAKEN <sogaken0919@gmail.com>
2023-11-30 18:12:46 +0900

    2in1

--- csvDiff/main.go
+++ csvDiff/main.go
@@ -2,110 +2,151 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
-func main() {
-	// 指定フォルダのファイル一覧を取得
+// Record represents a row in the CSV file.
+type Record struct {
+	Switch string
+	Inum   string
+}
+
+// findFiles finds the files that start with specified prefixes.
+func findFiles(prefix1, prefix2 string) (string, string, error) {
 	entries, err := os.ReadDir(".")
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
 
-	// CSVファイルを処理
+	var file1, file2 string
 	for _, entry := range entries {
-		if !entry.IsDir() && filepath.Ext(entry.Name()) == ".csv" {
-			processCSV(entry.Name(), "switch_call_id")
+		if entry.IsDir() {
+			continue
+		}
+
+		name := entry.Name()
+		if strings.HasPrefix(name, prefix1) && file1 == "" {
+			file1 = name
+		} else if strings.HasPrefix(name, prefix2) && file2 == "" {
+			file2 = name
 		}
 	}
+
+	if file1 == "" || file2 == "" {
+		return "", "", fmt.Errorf("could not find matching files")
+	}
+
+	return file1, file2, nil
 }
 
-func processCSV(fileName string, columnName string) {
-	// CSVファイルを開く
-	file, err := os.Open(fileName)
+// ReadCSV reads a CSV file and returns a map of records and the header.
+func ReadCSV(filename string) (map[string]Record, []string, error) {
+	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 	defer file.Close()
 
-	// CSVリーダーを作成
 	reader := csv.NewReader(file)
-
-	// 最初の行を読み込む（ヘッダーとして）
-	header, err := reader.Read()
-	if err != nil {
-		panic(err)
-	}
-
-	// カラム名からインデックスを見つける
-	columnIndex := -1
-	for i, col := range header {
-		if strings.ToLower(col) == strings.ToLower(columnName) {
-			columnIndex = i
-			break
-		}
-	}
-	if columnIndex == -1 {
-		panic("Column " + columnName + " not found in file " + fileName)
-	}
-
-	// 残りのCSVデータを読み込む
 	records, err := reader.ReadAll()
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
-	// レコードのカウントを保存するマップ
-	counts := make(map[string]int)
-	for _, record := range records {
-		if len(record) > columnIndex {
-			counts[record[columnIndex]]++
-		}
+	if len(records) == 0 {
+		return nil, nil, fmt.Errorf("file %s is empty", filename)
 	}
 
-	// ユニークと重複レコードを分類
-	uniqueRecords := make([][]string, 0)
-	duplicateRecords := make([][]string, 0)
-	for _, record := range records {
-		if len(record) > columnIndex {
-			if counts[record[columnIndex]] == 1 {
-				uniqueRecords = append(uniqueRecords, record)
-			} else {
-				duplicateRecords = append(duplicateRecords, record)
-			}
-		}
+	header := records[0]
+	recordMap := make(map[string]Record)
+	for _, record := range records[1:] {
+		key := record[0] + "-" + record[1] // Assuming 'switch' is in column 0 and 'inum' in column 1
+		recordMap[key] = Record{Switch: record[0], Inum: record[1]}
 	}
 
-	// ユニークなレコードのみのCSVを作成（ヘッダーを含む）
-	writeCSV(fileName+"_unique.csv", header, uniqueRecords)
-
-	// 重複するレコードのCSVを作成（ヘッダーを含む）
-	writeCSV(fileName+"_duplicates.csv", header, duplicateRecords)
+	return recordMap, header, nil
 }
 
-func writeCSV(fileName string, header []string, records [][]string) {
-	// 新しいCSVファイルを開く
-	file, err := os.Create(fileName)
+// writeCSV writes records to a CSV file, including the header.
+func writeCSV(filename string, records []Record, header []string) error {
+	file, err := os.Create(filename)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer file.Close()
 
-	// CSVライターを作成
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	// ヘッダーを書き込む
+	// Write the header
 	if err := writer.Write(header); err != nil {
-		panic(err)
+		return err
 	}
 
-	// レコードを書き込む
 	for _, record := range records {
-		if err := writer.Write(record); err != nil {
-			panic(err)
+		if err := writer.Write([]string{record.Switch, record.Inum}); err != nil {
+			return err
 		}
+	}
+
+	return nil
+}
+
+// CompareAndWrite compares two CSV files and writes the results to separate files.
+func CompareAndWrite(fileA, fileB string) error {
+	recordsA, headerA, err := ReadCSV(fileA)
+	if err != nil {
+		return err
+	}
+
+	recordsB, headerB, err := ReadCSV(fileB)
+	if err != nil {
+		return err
+	}
+
+	// Optionally, you can check if headers are the same in both files
+	if fmt.Sprintf("%v", headerA) != fmt.Sprintf("%v", headerB) {
+		return fmt.Errorf("headers do not match")
+	}
+
+	aOnly, bOnly, common := make([]Record, 0), make([]Record, 0), make([]Record, 0)
+
+	for key, record := range recordsA {
+		if _, exists := recordsB[key]; !exists {
+			aOnly = append(aOnly, record)
+		} else {
+			common = append(common, record)
+		}
+	}
+
+	for key, record := range recordsB {
+		if _, exists := recordsA[key]; !exists {
+			bOnly = append(bOnly, record)
+		}
+	}
+
+	// Define output file names based on input file names
+	prefixA := strings.TrimSuffix(fileA, ".csv")
+	prefixB := strings.TrimSuffix(fileB, ".csv")
+
+	// Write results to files
+	writeCSV(prefixA+"_a_only.csv", aOnly, headerA)
+	writeCSV(prefixB+"_b_only.csv", bOnly, headerB)
+	writeCSV(prefixA+"_common.csv", common, headerA)
+
+	return nil
+}
+
+func main() {
+	osakaFile, tokyoFile, err := findFiles("osaka_", "tokyo_")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if err := CompareAndWrite(osakaFile, tokyoFile); err != nil {
+		fmt.Println("Error:", err)
 	}
 }
