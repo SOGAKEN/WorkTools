68c697f258a1880d1ffe59eabbe49aff9c1f14ad
SOGAKEN <sogaken0919@gmail.com>
2023-12-03 01:04:05 +0900

    Complete

--- csvDiff/main.go
+++ csvDiff/main.go
@@ -4,57 +4,78 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
-// findFiles finds the files that start with specified prefixes.
-func findFiles(prefix1, prefix2 string) (string, string, error) {
-	entries, err := os.ReadDir(".")
+func main() {
+	folderPath := "." // Set the folder path here
+
+	csvFiles, err := findCSVFiles(folderPath)
 	if err != nil {
-		return "", "", err
+		fmt.Println("Error finding CSV files:", err)
+		return
 	}
 
-	var file1, file2 string
-	for _, entry := range entries {
-		if entry.IsDir() {
+	if len(csvFiles) < 2 {
+		fmt.Println("Need at least two CSV files for comparison")
+		return
+	}
+
+	// Read and store data from all CSV files
+	allRecords := make(map[string][][]string)
+	allHeaders := make(map[string][]string)
+	for _, file := range csvFiles {
+		records, header, err := readCSV(file)
+		if err != nil {
+			fmt.Println("Error reading file:", file, err)
 			continue
 		}
+		allRecords[file] = records
+		allHeaders[file] = header
+	}
 
-		name := entry.Name()
-		if strings.HasPrefix(name, prefix1) && file1 == "" {
-			file1 = name
-		} else if strings.HasPrefix(name, prefix2) && file2 == "" {
-			file2 = name
+	// Compare and classify records from the first two CSV files
+	file1 := csvFiles[0]
+	file2 := csvFiles[1]
+
+	switchIndex1 := findColumnIndex(allHeaders[file1], "switch")
+	switchIndex2 := findColumnIndex(allHeaders[file2], "switch")
+
+	if switchIndex1 == -1 || switchIndex2 == -1 {
+		fmt.Println("Error: 'switch' column not found in one or both files")
+		return
+	}
+
+	switches1 := getSwitchValues(allRecords[file1], switchIndex1)
+	switches2 := getSwitchValues(allRecords[file2], switchIndex2)
+
+	uniqueFile1, uniqueFile2, common := classifyRecords(switches1, switches2, allRecords[file1], allRecords[file2], switchIndex1, switchIndex2)
+
+	// Save results to CSV files with headers
+	saveCSV("unique_"+filepath.Base(file1), uniqueFile1, allHeaders[file1])
+	saveCSV("unique_"+filepath.Base(file2), uniqueFile2, allHeaders[file2])
+	saveCSV("common_records.csv", common, allHeaders[file1]) // Assuming both files have the same header structure
+}
+
+// findCSVFiles finds and returns all CSV files in the given directory
+func findCSVFiles(folderPath string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-	}
-
-	if file1 == "" || file2 == "" {
-		return "", "", fmt.Errorf("could not find matching files")
-	}
-
-	return file1, file2, nil
-}
-
-// getColumnIndex finds the index of a column in the header.
-func getColumnIndex(header []string, columnName string) (int, error) {
-	for i, h := range header {
-		if h == columnName {
-			return i, nil
+		if !info.IsDir() && filepath.Ext(path) == ".csv" {
+			files = append(files, path)
 		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
 	}
-	return -1, fmt.Errorf("column %s not found", columnName)
+	return files, nil
 }
 
-// trimDecimal trims the decimal part of the inum value.
-func trimDecimal(inum string) string {
-	if dot := strings.Index(inum, "."); dot != -1 {
-		return inum[:dot]
-	}
-	return inum
-}
-
-// ReadCSV reads a CSV file and returns a map of the records (entire row) and the header.
-func ReadCSV(filename string) (map[string][]string, []string, error) {
+func readCSV(filename string) ([][]string, []string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, nil, err
@@ -67,96 +88,81 @@ func ReadCSV(filename string) (map[string][]string, []string, error) {
 		return nil, nil, err
 	}
 
-	if len(records) < 2 {
-		return nil, nil, fmt.Errorf("file %s does not contain enough data", filename)
-	}
-
-	header := records[0]
-	switchIndex, err := getColumnIndex(header, "switch")
-	if err != nil {
-		return nil, nil, err
-	}
-	inumIndex, err := getColumnIndex(header, "inum")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	recordMap := make(map[string][]string)
-	for _, record := range records[1:] {
-		inum := trimDecimal(record[inumIndex])
-		key := record[switchIndex] + "-" + inum
-		recordMap[key] = record
-	}
-
-	return recordMap, header, nil
+	header := records[0] // First row is the header
+	data := records[1:]  // Remaining rows are data
+	return data, header, nil
 }
 
-// writeCSV writes records to a CSV file, including the header.
-func writeCSV(filename string, records [][]string, header []string) error {
+func findColumnIndex(header []string, columnName string) int {
+	for index, column := range header {
+		if column == columnName {
+			return index
+		}
+	}
+	return -1 // Column not found
+}
+
+func getSwitchValues(records [][]string, switchIndex int) map[string]bool {
+	switches := make(map[string]bool)
+	for _, record := range records {
+		if len(record) > switchIndex {
+			switches[record[switchIndex]] = true
+		}
+	}
+	return switches
+}
+
+// classifyRecords function modified to include duplicates in common records
+func classifyRecords(tokyoSwitches, osakaSwitches map[string]bool, tokyoRecords, osakaRecords [][]string, switchIndexTokyo, switchIndexOsaka int) ([][]string, [][]string, [][]string) {
+	uniqueTokyo := [][]string{}
+	uniqueOsaka := [][]string{}
+	common := [][]string{}
+
+	for _, record := range tokyoRecords {
+		if len(record) > switchIndexTokyo {
+			_, inOsaka := osakaSwitches[record[switchIndexTokyo]]
+			if !inOsaka {
+				uniqueTokyo = append(uniqueTokyo, record)
+			} else {
+				common = append(common, record) // Add to common if found in Osaka
+			}
+		}
+	}
+
+	for _, record := range osakaRecords {
+		if len(record) > switchIndexOsaka {
+			_, inTokyo := tokyoSwitches[record[switchIndexOsaka]]
+			if !inTokyo {
+				uniqueOsaka = append(uniqueOsaka, record)
+			} else {
+				common = append(common, record) // Add to common if found in Tokyo
+			}
+		}
+	}
+
+	return uniqueTokyo, uniqueOsaka, common
+}
+
+func saveCSV(filename string, records [][]string, header []string) {
 	file, err := os.Create(filename)
 	if err != nil {
-		return err
+		fmt.Println("Error creating file:", err)
+		return
 	}
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	if err := writer.Write(header); err != nil {
-		return err
+	if err := writer.Write(header); err != nil { // Write the header first
+		fmt.Println("Error writing header to file:", err)
+		return
 	}
 
 	for _, record := range records {
 		if err := writer.Write(record); err != nil {
-			return err
+			fmt.Println("Error writing record to file:", err)
+			return
 		}
-	}
-
-	return nil
-}
-
-// CompareFiles compares two CSV files and writes the results to separate files.
-// osaka_ ファイルを基準にして、tokyo_ ファイルを比較します。
-func CompareFiles(fileOsaka, fileTokyo string) error {
-	recordsOsaka, headerOsaka, err := ReadCSV(fileOsaka)
-	if err != nil {
-		return err
-	}
-
-	recordsTokyo, _, err := ReadCSV(fileTokyo) // headerTokyo は使用しないので、_ で無視
-	if err != nil {
-		return err
-	}
-
-	inTokyo, notInTokyo := make([][]string, 0), make([][]string, 0)
-
-	// osaka_ ファイルの各レコードに対して、tokyo_ ファイルに含まれているかを確認
-	for key, record := range recordsOsaka {
-		if _, exists := recordsTokyo[key]; exists {
-			inTokyo = append(inTokyo, record)
-		} else {
-			notInTokyo = append(notInTokyo, record)
-		}
-	}
-
-	// 出力ファイル名を定義
-	prefixOsaka := strings.TrimSuffix(fileOsaka, ".csv")
-
-	// 結果をファイルに書き出す
-	writeCSV(prefixOsaka+"_in_tokyo.csv", inTokyo, headerOsaka)
-	writeCSV(prefixOsaka+"_not_in_tokyo.csv", notInTokyo, headerOsaka)
-
-	return nil
-}
-
-func main() {
-	osakaFile, tokyoFile, err := findFiles("osaka_", "tokyo_")
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	if err := CompareFiles(osakaFile, tokyoFile); err != nil {
-		fmt.Println("Error:", err)
 	}
 }
