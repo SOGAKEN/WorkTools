5183c2fa2b0c280d486980ed3ea3b57c7a8cee96
SOGAKEN <sogaken0919@gmail.com>
2024-01-30 16:35:01 +0900

    Update eccelize

--- createTools/excelPass/main.go
+++ createTools/excelPass/main.go
@@ -1,54 +1,75 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "path/filepath"
+	"os"
+	"path/filepath"
 
-    "github.com/xuri/excelize/v2"
+	"log"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("パスワードを引数として指定してください")
-        os.Exit(1)
-    }
-    password := os.Args[1]
+	a := app.New()
+	w := a.NewWindow("パスワード入力")
 
-    err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if filepath.Ext(path) == ".xlsx" {
-            err := setPassword(path, password)
-            if err != nil {
-                fmt.Printf("ファイル '%s' にパスワードを設定中にエラーが発生しました: %v\n", path, err)
-            } else {
-                fmt.Printf("ファイル '%s' にパスワードが設定されました\n", path)
-            }
-        }
-        return nil
-    })
+	passwordEntry := widget.NewPasswordEntry()
+	passwordEntry.SetPlaceHolder("パスワードを入力")
 
-    if err != nil {
-        fmt.Printf("エラー: %v\n", err)
-        os.Exit(1)
-    }
+	submitButton := widget.NewButton("OK", func() {
+		password := passwordEntry.Text
+		w.Close()
+		// パスワードを使用してExcelファイルを保護する
+		protectExcelFiles(password)
+	})
+
+	w.SetContent(container.NewVBox(
+		passwordEntry,
+		submitButton,
+	))
+
+	w.ShowAndRun()
 }
 
-func setPassword(filePath, password string) error {
-    f, err := excelize.OpenFile(filePath)
-    if err != nil {
-        return err
-    }
-    // excelizeには現在のバージョンで直接的なパスワード設定の機能はないため、
-    // ファイルの保護機能など他の方法を検討する必要があります。
-    // 以下はファイル保護の例です。
-    options := excelize.SetSheetProtectionOptions{
-        Password: password,
-    }
-    for _, name := range f.GetSheetList() {
-        f.ProtectSheet(name, &options)
-    }
-    return f.Save()
+func protectExcelFiles(password string) {
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Ext(path) == ".xlsx" {
+			err := protectSheet(path, password)
+			if err != nil {
+				log.Printf("ファイル '%s' の保護中にエラーが発生しました: %v\n", path, err)
+			} else {
+				log.Printf("ファイル '%s' は保護されました\n", path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("エラー: %v\n", err)
+	}
+}
+
+func protectSheet(filePath, password string) error {
+	f, err := excelize.OpenFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	for _, name := range f.GetSheetList() {
+		// シートをパスワードで保護
+		err := f.ProtectSheet(name, &excelize.SheetProtectionOptions{
+			Password: password,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return f.SaveAs(filePath)
 }
