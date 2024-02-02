641154268707f93fade535693b67ee7aa65658c6
SOGAKEN <sogaken0919@gmail.com>
2024-02-03 08:56:09 +0900

    update

--- createTools/excelPass/main.go
+++ createTools/excelPass/main.go
@@ -1,77 +1,75 @@
 package main
 
 import (
+	"log"
 	"os"
 	"path/filepath"
 
-	"log"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"github.com/xuri/excelize/v2"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("パスワード入力")
-	w.Resize(fyne.NewSize(300, 200))
-
-	passwordEntry := widget.NewPasswordEntry()
-	passwordEntry.SetPlaceHolder("Enter Password")
-
-	submitButton := widget.NewButton("OK", func() {
-		password := passwordEntry.Text
-		w.Close()
-		// パスワードを使用してExcelファイルを保護する
-		protectExcelFiles(password)
-	})
-
-	w.SetContent(container.NewVBox(
-		passwordEntry,
-		submitButton,
-	))
-
-	w.ShowAndRun()
+func init() {
+	// ログの出力設定を初期化
+	log.SetOutput(os.Stderr)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-func protectExcelFiles(password string) {
-	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+func protectExcelFiles(path, password string) error {
+	excelFilesFound := false // Excelファイルの存在フラグ
+
+	// ディレクトリ内の全ての.xlsxファイルを検索
+	err := filepath.Walk(path, func(filePath string, info os.FileInfo, err error) error {
 		if err != nil {
+			log.Printf("パス %q の走査中にエラーが発生しました: %v\n", filePath, err)
 			return err
 		}
-		if filepath.Ext(path) == ".xlsx" {
-			err := protectSheet(path, password)
+		if !info.IsDir() && filepath.Ext(filePath) == ".xlsx" {
+			excelFilesFound = true // Excelファイルを見つけたらフラグをtrueに設定
+
+			// Excelファイルを開く
+			f, err := excelize.OpenFile(filePath)
 			if err != nil {
-				log.Printf("ファイル '%s' の保護中にエラーが発生しました: %v\n", path, err)
-			} else {
-				log.Printf("ファイル '%s' は保護されました\n", path)
+				log.Printf("ファイル %s を開く際にエラーが発生しました: %v\n", filePath, err)
+				return err
 			}
+
+			// ブックの保護を設定
+			if err := f.ProtectWorkbook(&excelize.WorkbookProtectionOptions{
+				Password:      password,
+				LockStructure: true,
+				LockWindows:   true,
+			}); err != nil {
+				log.Printf("ワークブック %s の保護設定時にエラーが発生しました: %v\n", filePath, err)
+				return err
+			}
+			// 保護を適用したファイルを保存
+			if err := f.Save(); err != nil {
+				log.Printf("保護されたワークブック %s を保存する際にエラーが発生しました: %v\n", filePath, err)
+				return err
+			}
+			log.Printf("保護されました: %s\n", filePath)
 		}
 		return nil
 	})
 
 	if err != nil {
-		log.Printf("エラー: %v\n", err)
-	}
-}
-
-func protectSheet(filePath, password string) error {
-	f, err := excelize.OpenFile(filePath)
-	if err != nil {
 		return err
 	}
 
-	for _, name := range f.GetSheetList() {
-		// シートをパスワードで保護
-		err := f.ProtectSheet(name, &excelize.SheetProtectionOptions{
-			Password: password,
-		})
-		if err != nil {
-			return err
-		}
+	// Excelファイルが一つも見つからなかった場合のログ出力
+	if !excelFilesFound {
+		log.Println("エクセルファイルがありません。")
 	}
 
-	return f.SaveAs(filePath)
+	return nil
+}
+
+func main() {
+	// 対象のディレクトリとパスワードを設定
+	dirPath := "./"             // ここには実行ファイルと同じ階層のディレクトリを指定
+	password := "your_password" // 保護に使用するパスワードを指定
+
+	if err := protectExcelFiles(dirPath, password); err != nil {
+		log.Printf("Excelファイルの保護中にエラーが発生しました: %v\n", err)
+	}
 }
