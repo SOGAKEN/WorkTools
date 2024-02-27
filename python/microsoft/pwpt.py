24d38c21ed1416dc2e4fd5fcc5e6e070d46eac91
SOGAKEN <sogaken0919@gmail.com>
2024-02-27 12:26:54 +0900

    Update file handling functions and add xlsx support.- Add openpyxl dependency for xlsx file handling

--- python/microsoft/pwpt.py
+++ python/microsoft/pwpt.py
@@ -8,78 +8,77 @@ import pythoncom  # COMライブラリの初期化とクリーンアップに必
 from datetime import datetime
 from pptx import Presentation
 from pptx.exc import PackageNotFoundError
+from openpyxl import load_workbook
+from openpyxl.utils.exceptions import InvalidFileException
 
 
 def get_application_path():
     """アプリケーションのパスを取得します。"""
-    if getattr(sys, 'frozen', False):
+    if getattr(sys, "frozen", False):
         path = os.path.dirname(sys.executable)
     else:
         path = os.path.dirname(os.path.abspath(__file__))
     return path
 
 
-def get_csv_path(base_name='process_results.csv'):
+def get_csv_path(base_name="process_results.csv"):
     """CSVファイルのパスを生成します。"""
     base_path = os.path.join(get_application_path(), base_name)
     if os.path.exists(base_path):
         timestamp = datetime.now().strftime("_%Y%m%d%H%M%S")
-        csv_path = base_path.replace('.csv', f'{timestamp}.csv')
+        csv_path = base_path.replace(".csv", f"{timestamp}.csv")
     else:
         csv_path = base_path
     return csv_path
 
 
-def can_open_pptx(filepath):
-    """指定されたPowerPointファイルが開けるかどうかを判定します。"""
+def can_open_file(filepath):
+    """指定されたファイルが開けるかどうかを判定します。"""
     try:
-        Presentation(filepath)
+        if filepath.endswith(".pptx"):
+            Presentation(filepath)
+        elif filepath.endswith(".xlsx"):
+            load_workbook(filepath)
         return True
-    except PackageNotFoundError:
+    except (PackageNotFoundError, InvalidFileException):
         # ファイルが開けない、またはパスワードで保護されている場合
         return False
 
 
-def set_document_or_presentation_readonly_with_timeout(filepath, edit_password, timeout=30):
-    """ドキュメントまたはプレゼンテーションを読み取り専用に設定します。タイムアウト機能付き。"""
+def set_write_password_with_timeout(filepath, edit_password, timeout=30):
+    """ファイルに書き込みパスワードを設定します。タイムアウト機能付き。"""
+
     def target(result_queue):
         pythoncom.CoInitialize()  # スレッドでCOMライブラリを初期化
         try:
-            if filepath.endswith('.docx'):
-                word = win32.gencache.EnsureDispatch('Word.Application')
+            if filepath.endswith(".docx"):
+                word = win32.gencache.EnsureDispatch("Word.Application")
                 doc = word.Documents.Open(filepath)
-                if doc.ProtectionType == win32.constants.wdNoProtection:
-                    doc.Protect(Type=win32.constants.wdAllowOnlyReading,
-                                NoReset=True, Password=edit_password)
-                    result_queue.put('OK')
-                else:
-                    result_queue.put('PASS')  # 既に保護されている場合
+                doc.Password = edit_password
+                result_queue.put("OK")
                 doc.Save()
                 doc.Close(False)
                 word.Quit()
-            elif filepath.endswith('.pptx'):
+            elif filepath.endswith(".pptx"):
                 powerpoint = win32.gencache.EnsureDispatch(
-                    'PowerPoint.Application')
-                try:
-                    presentation = powerpoint.Presentations.Open(
-                        filepath, WithWindow=False)
-                    try:
-                        # 書き込みパスワードを設定
-                        presentation.WritePassword = edit_password
-                        # ファイルを保存して閉じる
-                        presentation.Save()
-                        result_queue.put('OK')
-                    except Exception as e:
-                        result_queue.put('PASS')
-                    finally:
-                        presentation.Close()
-                except Exception as e:
-                    # PowerPointファイルがパスワードで保護されている場合、ここで例外が発生
-                    result_queue.put('PASS')
-                finally:
-                    powerpoint.Quit()
+                    "PowerPoint.Application")
+                presentation = powerpoint.Presentations.Open(
+                    filepath, WithWindow=False)
+                presentation.WritePassword = edit_password
+                result_queue.put("OK")
+                presentation.Save()
+                presentation.Close()
+                powerpoint.Quit()
+            elif filepath.endswith(".xlsx"):
+                excel = win32.gencache.EnsureDispatch("Excel.Application")
+                workbook = excel.Workbooks.Open(filepath)
+                workbook.Password = edit_password
+                result_queue.put("OK")
+                workbook.Save()
+                workbook.Close(False)
+                excel.Quit()
         except Exception as e:
-            result_queue.put('NG')
+            result_queue.put("NG")
         finally:
             pythoncom.CoUninitialize()
 
@@ -89,30 +88,45 @@ def set_document_or_presentation_readonly_with_timeout(filepath, edit_password, 
     thread.join(timeout)
     if thread.is_alive():
         thread.join()  # Ensure thread has finished
-        return 'TIMEOUT'
+        return "TIMEOUT"
     return result_queue.get()
 
 
 def process_directory_for_documents(directory, edit_password):
-    """ディレクトリ内のdocxおよびpptxファイルを処理します。"""
+    """ディレクトリ内のファイルを処理します。"""
     results = []
-    total_files = sum([len(files) for _, _, files in os.walk(directory) if any(
-        file.endswith(('.docx', '.pptx')) for file in files)])
+    total_files = sum(
+        [
+            len(files)
+            for _, _, files in os.walk(directory)
+            if any(file.endswith((".docx", ".pptx", ".xlsx")) for file in files)
+        ]
+    )
     print(f"合計で処理するファイルの数: {total_files}")
 
     file_count = 0
     for root, _, files in os.walk(directory):
-        for file in filter(lambda f: f.endswith('.docx') or f.endswith('.pptx'), files):
+        for file in filter(
+            lambda f: f.endswith(".docx") or f.endswith(
+                ".pptx") or f.endswith(".xlsx"),
+            files,
+        ):
             filepath = os.path.join(root, file)
-            if filepath.endswith('.pptx') and not can_open_pptx(filepath):
+            if not can_open_file(filepath):
                 print(f"ファイルが開けないためスキップします: {filepath}")
-                results.append({'NAME': os.path.basename(filepath),
-                                'RESULT': 'SKIP', 'PATH': filepath})
-                continue  # ファイルが開けない場合はスキップ
-            result = set_document_or_presentation_readonly_with_timeout(
-                filepath, edit_password)
-            results.append({'NAME': os.path.basename(filepath),
-                           'RESULT': result, 'PATH': filepath})
+                results.append(
+                    {
+                        "NAME": os.path.basename(filepath),
+                        "RESULT": "SKIP",
+                        "PATH": filepath,
+                    }
+                )
+                continue
+            result = set_write_password_with_timeout(filepath, edit_password)
+            results.append(
+                {"NAME": os.path.basename(
+                    filepath), "RESULT": result, "PATH": filepath}
+            )
             file_count += 1
             print_progress(file, result, file_count, total_files)
 
@@ -129,8 +143,8 @@ def print_progress(file_name, result, file_count, total_files):
 
 def write_results_to_csv(results, csv_path):
     """結果をCSVに書き込みます。"""
-    with open(csv_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
-        fieldnames = ['NAME', 'RESULT', 'PATH']
+    with open(csv_path, "w", newline="", encoding="utf-8-sig") as csvfile:
+        fieldnames = ["NAME", "RESULT", "PATH"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for result in results:
@@ -138,14 +152,14 @@ def write_results_to_csv(results, csv_path):
     print(f"CSVに結果を書き込みました: {csv_path}")
 
 
-if __name__ == '__main__':
-    edit_password = 'your_edit_password'  # ここに適切なパスワードを設定してください
+if __name__ == "__main__":
+    edit_password = "your_edit_password"  # ここに適切なパスワードを設定してください
     current_directory = get_application_path()
     results = process_directory_for_documents(current_directory, edit_password)
     if results:
         output_csv_path = get_csv_path()
         write_results_to_csv(results, output_csv_path)
-        print(f'処理結果は{output_csv_path}に保存されました。')
+        print(f"処理結果は{output_csv_path}に保存されました。")
     else:
         print("処理するファイルが見つかりませんでした。")
 
