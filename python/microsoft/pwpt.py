31416644c5e3aa16153ce08f0559dd55d207ade7
SOGAKEN <sogaken0919@gmail.com>
2024-02-27 13:27:29 +0900

    Add function to set password for Office files

--- python/microsoft/pwpt.py
+++ python/microsoft/pwpt.py
@@ -3,49 +3,55 @@ import csv
 import win32com.client
 import pythoncom
 from threading import Timer
+from queue import Queue
 
 
-def set_password(file_path, password):
+def set_password_office(file_path, password, result_queue):
+    pythoncom.CoInitialize()
+    extension = os.path.splitext(file_path)[1].lower()
     try:
-        pythoncom.CoInitialize()
-        extension = os.path.splitext(file_path)[1].lower()
         if extension == ".pptx":
-            app = win32com.client.Dispatch("PowerPoint.Application")
-            presentation = app.Presentations.Open(file_path, WithWindow=False)
-            presentation.SaveAs(file_path, Password=password)
-            presentation.Close()
-            app.Quit()
+            app = win32com.client.gencache.EnsureDispatch(
+                "PowerPoint.Application")
+            doc = app.Presentations.Open(file_path, WithWindow=False)
+            doc.WritePassword = password
+            doc.Save()
+            doc.Close()
         elif extension == ".docx":
-            app = win32com.client.Dispatch("Word.Application")
-            document = app.Documents.Open(file_path)
-            document.SaveAs2(file_path, WritePassword=password)
-            document.Close()
-            app.Quit()
+            app = win32com.client.gencache.EnsureDispatch("Word.Application")
+            doc = app.Documents.Open(file_path)
+            doc.WritePassword = password
+            doc.SaveAs2(file_path, WritePassword=password)
+            doc.Close()
         elif extension == ".xlsx":
-            app = win32com.client.Dispatch("Excel.Application")
-            workbook = app.Workbooks.Open(file_path)
-            workbook.SaveAs(file_path, WriteResPassword=password)
-            workbook.Close()
-            app.Quit()
+            app = win32com.client.gencache.EnsureDispatch("Excel.Application")
+            doc = app.Workbooks.Open(file_path)
+            doc.Password = password
+            doc.SaveAs(file_path, WriteResPassword=password)
+            doc.Close()
         else:
-            return "NG", "Unsupported file type"
-        return "OK", "Password set successfully"
+            result_queue.put(("NG", "Unsupported file type"))
+            return
+        app.Quit()
+        result_queue.put(("OK", "Password set successfully"))
     except Exception as e:
-        return "NG", str(e)
+        result_queue.put(
+            ("PASS", "File is password protected or another error occurred")
+        )
     finally:
         pythoncom.CoUninitialize()
 
 
 def process_file(file_path, password):
-    timer = Timer(10.0, lambda: os._exit(1))
+    result_queue = Queue()
+    timer = Timer(10.0, lambda: result_queue.put(
+        ("TIMEOUT", "Operation timed out")))
     try:
         timer.start()
-        result, message = set_password(file_path, password)
-    except SystemExit:
-        return file_path, "TIMEOUT"
+        set_password_office(file_path, password, result_queue)
     finally:
         timer.cancel()
-    return file_path, result
+    return file_path, result_queue.get()
 
 
 def process_directory_for_documents(directory, password):
