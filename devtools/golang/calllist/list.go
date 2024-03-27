12e508b4a7fc3beef7257f52b2c62be7a35da0c7
SOGAKEN <sogaken0919@gmail.com>
2024-03-27 20:12:33 +0900

    Refactor cell value retrieval and processing logic

--- devtools/golang/calllist/list.go
+++ devtools/golang/calllist/list.go
@@ -24,24 +24,29 @@ func main() {
 	var results []int // 結果を格納するためのスライス
 
 	for i := 1; ; i++ { // A列が空になるまで繰り返し
-		cellA, _ := f.GetCellValue("Sheet1", fmt.Sprintf("A%d", i))
-		if cellA == "" {
-			break
+		cellA, errA := f.GetCellValue("Sheet1", fmt.Sprintf("A%d", i))
+		if errA != nil || cellA == "" {
+			break // エラーまたはA列が空なら処理を終了
 		}
 
-		cellB, _ := f.GetCellValue("Sheet1", fmt.Sprintf("B%d", i))
-		if cellA == "Equal To" {
-			value, _ := strconv.Atoi(cellB)
-			results = append(results, value) // Equal Toの場合、B列の値を直接追加
-			continue
+		cellB, errB := f.GetCellValue("Sheet1", fmt.Sprintf("B%d", i))
+		if errB != nil {
+			continue // B列の読み取りに失敗した場合はスキップ
 		}
 
-		cellC, _ := f.GetCellValue("Sheet1", fmt.Sprintf("C%d", i))
-		if cellA == "In Range" {
-			start, _ := strconv.Atoi(cellB)
-			end, _ := strconv.Atoi(cellC)
-			for j := start; j <= end; j++ {
-				results = append(results, j) // In Rangeの場合、範囲内の全数値を追加
+		switch cellA {
+		case "In Range":
+			start, errStart := strconv.Atoi(cellB)
+			cellC, _ := f.GetCellValue("Sheet1", fmt.Sprintf("C%d", i))
+			end, errEnd := strconv.Atoi(cellC)
+			if errStart == nil && errEnd == nil {
+				for j := start; j <= end; j++ {
+					results = append(results, j) // In Rangeの場合、範囲内の全数値を追加
+				}
+			}
+		case "Equal To":
+			if value, err := strconv.Atoi(cellB); err == nil {
+				results = append(results, value) // Equal Toの場合、B列の値を直接追加
 			}
 		}
 	}
